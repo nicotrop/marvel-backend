766500b2765fe213237274c7c95444cff58fe8fb
Nicolas Troplent <nicolastroplent@Nicolass-MacBook-Pro.local>
2022-05-30 10:32:16 +0200

    fixed favorites routes

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose")
 
 const Favorites = require("../models/Favorites");
 const User = require("../models/User");
@@ -8,8 +9,8 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 //Adding a favorite
 router.post("/favorite/add", isAuthenticated, async (req, res) => {
   const { title, path, extension, description, elementID } = req.fields;
-  console.log(req.fields);
   try {
+    const favs = await Favorites({elementID: elementID, owner: req.user_id})
     const newFavorite = new Favorites({
       title: title,
       path: path,
@@ -26,37 +27,33 @@ router.post("/favorite/add", isAuthenticated, async (req, res) => {
 });
 
 //Remove favorite
-router.delete("/favorite/delete", async (req, res) => {
-  const token = req.fields.token;
+router.post("/favorite/delete", isAuthenticated, async (req, res) => {
+  const { elementID } = req.fields;
   try {
-    const user = await User.findOne({ token: token });
-    await Favorites.findOneAndDelete({
-      elementID: req.fields.id,
-      owner: user._id,
+    const deletedMarvel = await Favorites.findOneAndDelete({
+      elementID: elementID,
+      owner: req.user._id,
     });
-    res.status(200).json({ success: "removed from the playlist" });
+    console.log(deletedMarvel);
+    res.status(200).json({ success: "removed from the list" });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
 //Get favorites
-router.post("/favorite", async (req, res) => {
-  const token = req.fields.token;
-  if (token) {
+router.post("/favorite/list", isAuthenticated ,async (req, res) => {
+  // const token = req.fields.token;
+  console.log(req.user);
+  if (req.user) {
     try {
-      const user = await User.findOne({ token: token });
-      if (user) {
-        const list = await Favorites.find({ owner: user._id });
-        res.status(200).json(list);
-      } else {
-        res.status(400).json({ message: "could not find user" });
-      }
+      const list = await Favorites.find({owner: req.user._id});
+      res.status(200).json(list);
     } catch (error) {
       res.status(400).json(error.message);
     }
   } else {
-    res.status(400).json({ message: "no token" });
+    res.status(400).json({ message: "Could not find user" });
   }
 });
 
